4780e643b8bd20df0b2afd08a7e95da17b0bc0a1
Yujihyeee <rladnddl@naver.com>
2021-11-01 20:40:51 +0900

    20

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { AdminPage } from 'admin/index';
+import { DashBoard } from 'analysis/index';
+import { UserList } from 'user/index';
+import { SalesManagement } from 'sales/index';
+import { FinancialReports } from 'financial/index';
+
+
+
+const App = () => (
+  <>
+    <Switch>
+      <Route exact path='/' component={AdminLogin} />
+      <Redirect from='/admin-page' to={'/'} />
+      <Route exact path='/dash-board' component={DashBoard} />
+      <Route exact path='/user-list' component={UserList} />
+      <Route exact path='/sales-management' component={SalesManagement} />
+      <Route exact path='/financial-reports' component={FinancialReports} />
+    </Switch>
+  </>
+)
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,21 @@
-// Internet Explorer 11 requires polyfills and partially supported by this project.
-// import 'react-app-polyfill/ie11';
-// import 'react-app-polyfill/stable';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './i18n';
-import './styles/app-base.css';
-import './styles/app-components.css';
-import './styles/app-utilities.css';
-import App from 'app/App';
+import './index.css';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
