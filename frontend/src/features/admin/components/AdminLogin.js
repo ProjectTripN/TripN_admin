502087d60be11ebec379cef24d3a9f076bba9275
Yujihyeee <rladnddl@naver.com>
2021-11-02 18:23:04 +0900

    18

--- frontend/src/features/admin/components/AdminLogin.js
+++ frontend/src/features/admin/components/AdminLogin.js
@@ -1,123 +1,128 @@
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  Link,
-  TextField,
-  Typography
-} from '@material-ui/core';
+import { useForm } from "react-hook-form";
+import { Link as useNavigate } from 'react-router-dom';
+import { Box, Button, Container, Grid, TextField, Typography } from '@material-ui/core';
 import { Navigation } from 'features/common';
+import { loginPage } from '../reducers/adminSlice';
 
-export default function AdminLogin () {
-  
-  return (
-    <>
-      <Helmet>
-        <title>Login | Material Kit</title>
-      </Helmet>
-      <Box
-        sx={{
-          backgroundColor: 'background.default',
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-          justifyContent: 'center'
-        }}
-      >
-        <Container maxWidth="sm">
-          <Formik
-            initialValues={{
-              email: 'demo@devias.io',
-              password: 'Password123'
-            }}
-            validationSchema={Yup.object().shape({
-              email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
-              password: Yup.string().max(255).required('Password is required')
-            })}
-            onSubmit={() => {
-              Navigation('/', { replace: true });
-            }}
-          >
-            {({
-              errors,
-              handleBlur,
-              handleChange,
-              handleSubmit,
-              isSubmitting,
-              touched,
-              values
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <Box sx={{ mb: 3 }}>
-                  <Typography
-                    color="textPrimary"
-                    variant="h2"
-                  >
-                    Admin Page
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    pb: 1,
-                    pt: 3
-                  }}
+export default function AdminLogin() {
+  // const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { handleSubmit, formState: { errors } } = useForm();
+
+  return (<>
+      <title>Login | Admin Page</title>
+    <Box
+      sx={{
+        backgroundColor: 'background.default',
+        display: 'flex',
+        flexDirection: 'column',
+        height: 'center',
+        justifyContent: 'center'
+      }}>
+      <Container maxWidth="sm">
+        <Formik
+          initialValues={{
+            email: 'demo@devias.io',
+            password: 'Password123'
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string().email('Must be a valid email').max(50).required('Email is required'),
+            password: Yup.string().max(15).required('Password is required')
+          })}
+          onSubmit={() => {
+            Navigation('/', { replace: true });
+          }}>
+          {({
+            errors,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+            touched,
+            values
+          }) => (
+            <form method='POST'
+              onSubmit={handleSubmit(async (data) => {await dispatch(loginPage(data))})}>
+              <Box sx={{ mb: 3 }}>
+                <Typography
+                  color="textPrimary"
+                  variant="h3"
+                  align="center">
+                  Admin Page
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  pb: 1,
+                  pt: 3
+                }}>
+                <Typography
+                  align="center"
+                  color="textSecondary"
+                  variant="body1">
+                  Sign in with email address
+                </Typography>
+              </Box>
+              <Grid
+                container
+                spacing={3}
+              >
+                <Grid
+                  item
+                  xs={12}
+                  md={6}
                 >
-                  <Typography
-                    align="center"
-                    color="textSecondary"
-                    variant="body1"
-                  >
-                    login with email address
-                  </Typography>
-                </Box>
-                <TextField
-                  error={Boolean(touched.email && errors.email)}
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  md={6}
+                >
+                </Grid>
+              </Grid>
+              <TextField
+                error={Boolean(touched.email && errors.email)}
+                fullWidth
+                helperText={touched.email && errors.email}
+                label="Email Address"
+                margin="normal"
+                name="email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="email"
+                value={values.email}
+                variant="outlined" />
+              <TextField
+                error={Boolean(touched.password && errors.password)}
+                fullWidth
+                helperText={touched.password && errors.password}
+                label="Password"
+                margin="normal"
+                name="password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="password"
+                value={values.password}
+                variant="outlined" />
+              <Box sx={{ py: 2 }}>
+                <Button
+                  color="primary"
+                  disabled={isSubmitting}
                   fullWidth
-                  helperText={touched.email && errors.email}
-                  label="Email Address"
-                  margin="normal"
-                  name="email"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="email"
-                  value={values.email}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.password && errors.password)}
-                  fullWidth
-                  helperText={touched.password && errors.password}
-                  label="Password"
-                  margin="normal"
-                  name="password"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="password"
-                  value={values.password}
-                  variant="outlined"
-                />
-                <Box sx={{ py: 2 }}>
-                  <Button
-                    color="primary"
-                    disabled={isSubmitting}
-                    fullWidth
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                  >
-                    Sign in
-                  </Button>
-                </Box>
-              </form>
-            )}
-          </Formik>
-        </Container>
-      </Box>
-    </>
-  );
+                  size="large"
+                  type="submit"
+                  variant="contained">
+                  Sign in
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
+      </Container>
+    </Box>
+  </>);
 };
--- frontend/src/features/admin/index.js
+++ frontend/src/features/admin/index.js
@@ -1,1 +1,5 @@
 export {default as AdminLogin} from 'features/admin/components/AdminLogin'
+
+export {default as adminReducer} from 'features/admin/reducers/adminSlice'
+
+export {default as adminAPI} from 'features/admin/reducers/adminAPI'
