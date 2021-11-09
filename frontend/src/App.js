abbb3eabbc724cdb2a94e349051fdca2aff5751e
Yujihyeee <rladnddl@naver.com>
2021-11-10 02:35:21 +0900

    2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { DashBoard } from 'features/common';
+import { Dashboard } from 'features/common';
 import { UserList } from 'features/user';
 import { SalesManagement, FinancialReports } from 'features/financial';
 import { AdminLogin } from 'features/admin';
@@ -13,7 +13,7 @@ const App = () => {
         
         <Route path='/an/admin-login' element={<AdminLogin />} />
         <Route path='/' element={<Navigate replace to='/an/admin-login' />} />
-        <Route path='/an/dash-board' element={<DashBoard />} />
+        <Route path='/an/dash-board' element={<Dashboard />} />
         <Route path='/an/user-list' element={<UserList />} />
         <Route path='/an/sales-management' element={<SalesManagement />} />
         <Route path='/an/financial-reports' element={<FinancialReports />} />
--- frontend/src/features/common/components/Dashboard.js
+++ frontend/src/features/common/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet';
 import { Box, Container, Grid } from '@material-ui/core';
-import { Budget, LatestOrders, LatestProducts, Sales, TasksProgress, TotalCustomers, TotalProfit, TrafficByDevice } from 'features/common'
+import { Budget, LatestOrders, Sales, TasksProgress, TotalCustomers, TotalProfit, TrafficByDevice } from 'features/common'
 
 const Dashboard = () => (
     <>
@@ -76,19 +76,10 @@ const Dashboard = () => (
             </Grid>
             <Grid
               item
-              lg={4}
-              md={6}
-              xl={3}
-              xs={12}
-            >
-              <LatestProducts sx={{ height: '100%' }} />
-            </Grid>
-            <Grid
-              item
-              lg={8}
-              md={12}
-              xl={9}
-              xs={12}
+              lg={12}
+              md={18}
+              xl={12}
+              xs={24}
             >
               <LatestOrders />
             </Grid>
