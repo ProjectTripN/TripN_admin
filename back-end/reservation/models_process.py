a45f3ad5ac4180d603c489f8627480e06d297d97
Yujihyeee <rladnddl@naver.com>
2021-12-15 14:31:18 +0900

    14

--- back-end/reservation/models_process.py
+++ back-end/reservation/models_process.py
@@ -3,7 +3,7 @@ import csv
 from datetime import datetime
 import pandas as pd
 from dateutil.relativedelta import relativedelta
-from rest_framework.generics import get_object_or_404
+from django.shortcuts import get_object_or_404
 from brevity.models import JejuSchedule
 from reservation.models import Reservation
 from jeju_data.models import Accommodation, Plane, Activity
@@ -24,43 +24,52 @@ class Processing:
 
     def pre_process(self):
         arr = []
-        for p in range(1, 12):
-            pr = JejuSchedule.objects.get(pk=1)
-            acc_pr = Accommodation.objects.get(pk=1)
-            plane = Plane.objects.get(pk=1)
-            activity = Activity.objects.get(pk=1)
+        for p in range(3, 14):
+            print(f' 유저아이디: {p}')
+            pr = JejuSchedule.objects.get(pk=p)
+            print('-------------------------')
+            print(pr)
+            acc_pr = Accommodation.objects.get(pk=p)
+            plane = Plane.objects.get(pk=p)
+            activity = Activity.objects.get(pk=p)
             people = pr.people
             day = pr.day
-            plane_pr = get_object_or_404(plane, id([]))
-            activity = get_object_or_404(activity, id([]))
+            plane_pr = plane.economyCharge
+            activity_pr = activity.price
             reg_date = pr.startday - relativedelta(days=10)
-            price = plane_pr + acc_pr.price + activity
+            price = plane_pr + acc_pr.price + activity_pr
             tax = (price * people) + (price * day) * 0.1
             subtotal = price + tax
             fee = subtotal * 0.2
             total_price = subtotal + fee
-            print(p)
+            jeju_schedule_id = p
             arr.append(reg_date)
             arr.append(price)
             arr.append(int(tax))
             arr.append(int(subtotal))
             arr.append(int(fee))
             arr.append(int(total_price))
-            print(arr)
-        result = 0
-        df = pd.DataFrame(result, columns=['reg_date', 'price', 'tax', 'subtotal', 'fees', 'total_price'])
+            arr.append(jeju_schedule_id)
+        n = 7
+        result = [arr[i * n:(i + 1) * n] for i in range((len(arr) + n - 1) // n)]
+        df = pd.DataFrame(result, columns=['reg_date', 'price', 'tax', 'subtotal', 'fees', 'total_price', 'jeju_schedule_id'])
+        print(df)
         df.to_csv(self.csvfile + 'price.csv')
 
     def insert_reservation(self):
         with open(self.csvfile, newline='', encoding='utf8') as f:
             data_reader = csv.DictReader(f)
+
             for row in data_reader:
+                # j = JejuSchedule()
+                # jeju_schedule_id = JejuSchedule.objects.filter(id=row['jeju_schedule_id']).values()[0]
+                # j.id = jeju_schedule_id['id']
                 reservation = Reservation.objects.create(reg_date=row['reg_date'],
                                                          price=row['price'],
                                                          tax=row['tax'],
                                                          subtotal=row['subtotal'],
                                                          fees=row['fees'],
                                                          total_price=row['total_price'],
-                                                         brevity_id=1)
+                                                         jeju_schedule_id=row['jeju_schedule_id'])
                 print(f'2 >>>> {reservation}')
             print('DATA UPLOADED SUCCESSFULLY!')
