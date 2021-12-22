07db62628b59979d03522e9f31d3de80f2508157
Yujihyeee <rladnddl@naver.com>
2021-12-22 12:02:31 +0900

    12

--- back-end/fin_reports/views.py
+++ back-end/fin_reports/views.py
@@ -1,7 +1,12 @@
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view, parser_classes
+from fin_reports.models import FinReports
 from fin_reports.models_data import DbUploader
+from fin_reports.serializers import FinReportsSerializer
+from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
 
 
 @api_view(['GET'])
@@ -18,8 +23,14 @@ def upload(request):
     return JsonResponse({'Product Upload': 'SUCCESS'})
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 @parser_classes([JSONParser])
-def process(request):
-    DbUploader().process()
-    return JsonResponse({'process': 'SUCCESS'})
+def find_by_detail(request):
+    print('############ 2 ##########')
+    quest = request.data
+    print(quest)
+    answer = FinReports.objects\
+        .filter(symptom=quest['symptom'], details=quest['details']).get()
+        # .only('symptom', 'level', 'answer')
+    serializer = FinReportsSerializer(answer)
+    return JsonResponse(data=serializer.data, safe=False)
