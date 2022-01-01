818ca28bacf9f579788063b26dba8ee2375ccb8d
Yujihyeee <rladnddl@naver.com>
2022-01-01 21:56:48 +0900

    21

--- backend/ledger/urls.py
+++ backend/ledger/urls.py
@@ -8,7 +8,7 @@ urlpatterns = {
     url(r'up_cost', views.upload_cost),
     url(r'profit', views.profit),
     url(r'report', views.report_process),
-    url(r'6month_cost', views.show_6month_cost),
+    url(r'6month', views.show_6month_cost),
     url(r'month_cost', views.month_cost),
     url(r'year_profit', views.year_profit),
     # url(r'sales/(?P<pk>\w{0,50})$', views.sales),
