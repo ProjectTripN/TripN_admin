feedcd81c3ce51e6aeecf7aca5832324a3f91689
Yujihyeee <rladnddl@naver.com>
2022-01-01 17:13:29 +0900

    17

--- backend/fin_reports/urls.py
+++ backend/fin_reports/urls.py
@@ -4,5 +4,5 @@ from fin_reports import views
 urlpatterns = {
     url(r'upload', views.upload),
     url(r'show_fin_reports', views.show_fin_reports),
-    url(r'fin_reports', views.fin_reports),
+    url(r'reports', views.fin_reports),
 }
