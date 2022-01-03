c297386ccb327fbe8c4363056bca3499b925c7cf
Yujihyeee <rladnddl@naver.com>
2022-01-03 22:28:23 +0900

    22

--- backend/fin_reports/models_process.py
+++ backend/fin_reports/models_process.py
@@ -1,5 +1,4 @@
 import csv
-
 import pandas as pd
 from django.db.models import Sum
 from fin_reports.models import FinReports
@@ -23,7 +22,7 @@ class ReportProcess:
         df1.insert(0, '항목명', ['매출액', '매출원가','매출총이익','판매비와관리비','지급수수료','영업이익',
                               '기타수익','기타비용','금융수익','금융비용'], True)
         # df1.to_csv('fin_reports/data/report.csv')
-        # with open('fin_reports/data/report.csv', newline='', encoding='utf8') as f:
+        # with open('fin_reports/data/reportreport.csv', newline='', encoding='utf8') as f:
         #     data_reader = csv.DictReader(f)
         #     for row in data_reader:
         #         report = FinReports.objects.create(year=2021,
@@ -32,18 +31,18 @@ class ReportProcess:
         #                                            )
         #         print(f'1 >>>> {report}')
         # print('USER DATA UPLOADED SUCCESSFULLY!')
-        i = FinReports.objects.filter(year=2021, category__in=['영업이익', '기타수익', '기타비용', '금융수익', '금융비용']).values()
-        i = i.values('price')
-        df = pd.DataFrame(i)
-        price = df.loc[0] + df.loc[1] - df.loc[2] + df.loc[3] - df.loc[4]
-        p = df.loc[1] - df.loc[2] + df.loc[3] - df.loc[4]
-        print(price, p)
-        report = FinReports.objects.create(year=2021,
-                                           category='기타손익 및 금융손익',
-                                           price=int(22010172),
-                                           )
-        print(f'1 >>>> {report}')
-        print('USER DATA UPLOADED SUCCESSFULLY!')
+        # i = FinReports.objects.filter(year=2021, category__in=['영업이익', '기타수익', '기타비용', '금융수익', '금융비용']).values()
+        # i = i.values('price')
+        # df = pd.DataFrame(i)
+        # price = df.loc[0] + df.loc[1] - df.loc[2] + df.loc[3] - df.loc[4]
+        # p = df.loc[1] - df.loc[2] + df.loc[3] - df.loc[4]
+        # print(price, p)
+        # report = FinReports.objects.create(year=2021,
+        #                                    category='당기순이익',
+        #                                    price=int(1157095884),
+        #                                    )
+        # print(f'1 >>>> {report}')
+        # print('USER DATA UPLOADED SUCCESSFULLY!')
 
 # 	    매출액  Sales
 # 	- 매출원가  Cost of sales
