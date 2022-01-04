d7206a8e6572a28a3ec8a213ef321fb324ff91ee
Yujihyeee <rladnddl@naver.com>
2022-01-04 17:37:28 +0900

    17

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://192.168.0.107:8000']
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://192.168.0.4:8000']
 CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
